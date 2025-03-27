bbe5b64f723103f3a62e0e69884265c61c70233c
DeMarkiz <markberezhnoy27@gmail.com>
2025-03-27 19:02:18 +0300

    static

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -108,9 +108,9 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-LOGIN_REDIRECT_URL = "/"
-
-LOGIN_URL = "/"
+# LOGIN_REDIRECT_URL = "/"
+#
+# LOGIN_URL = "/"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.yandex.ru"
